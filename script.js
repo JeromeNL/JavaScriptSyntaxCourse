4c731a839b9ece0fe0081c2c10dfbde4ac38299b
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-06 13:38:37 +0100

    Strings

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ Comment
  */
 
 /* JS data types
-undefined: Not defined
+undefined: Not defined (example: 'var a;'
 Null: nothing
 Boolean: True || False
 string: text
@@ -28,3 +28,92 @@ let ourName = "NL";
 const pi = 3.14;
 
 // And all lines in JS with ;, recommended
+
+// print stuff
+console.log(pi);
+
+// Math - same as other languages
+var sum = 10 + 10;
+// increment
+myVar3 = 5;
+myVar3 = myVar3 + 1;
+myVar3++; // also supported in JS :D
+
+//Decimal numbers (floats)
+var MyFloat = 2.5
+var myFloat2 = 4.4 / 2.0;
+
+// Find remainder
+var remain = 11 % 3; //result 2
+var value = 10
+value += 5; // also supported
+
+
+// Strings
+var firstName = "Alan";
+var lastname = "Turing";
+
+// Escape quotes
+var escaping = "I am \"a programmer\"";
+console.log(escaping)
+
+var escaping2 = 'I am "a programmer"';
+
+var myMulti = "FirstLine\nSecondLine"
+
+// Concat
+var concatString = "I am first " + "I am second";
+concatString += " I am Third";
+
+// Length string
+var length = concatString.length;
+console.log(length);
+
+// 0-based indexing
+console.log(concatString[3]);
+
+// strings cannot be changed
+//concatString[0]= h // does not work
+
+var lastLetter = "string";
+var lastLetterOfString = lastLetter[lastLetter.length -1];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
