ed23bd6b6c29a170a3ff56071626345228958192
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-06 12:59:33 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// in-line comment
+
+/*
+Multi
+Line
+Comment
+ */
+
+/* JS data types
+undefined: Not defined
+Null: nothing
+Boolean: True || False
+string: text
+symbol: char in C#/Java
+number: just a number
+object: an object, same as C#
+ */
+
+// use other data type later on
+// var: whole program
+var myName = "Jerome";
+myName = 8;
+
+// let: in scope
+let ourName = "NL";
+
+// const: constant, cannot be changed
+const pi = 3.14;
+
+// And all lines in JS with ;, recommended
