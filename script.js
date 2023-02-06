08990b7f70751f22d7ff577f43e0cbb5c63c4531
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-06 13:47:33 +0100

    Arrays

--- script.js
+++ script.js
@@ -78,11 +78,28 @@ console.log(concatString[3]);
 var lastLetter = "string";
 var lastLetterOfString = lastLetter[lastLetter.length -1];
 
+// array - can contain different data types
+var ourArray = ["John", 23];
 
+var TwoDArray = [[12, 15], ["Jerome", "NL"]]
+var age = TwoDArray[0][1];
+var name = TwoDArray[1][0];
+console.log(age + " " + name);
 
+TwoDArray[0][2] = 25;
+TwoDArray.push(["dog", 2]);
+console.log(TwoDArray[2][0]);
 
+// remove last
+TwoDArray.pop();
 
+// remove first
+TwoDArray.shift();
 
+// adds element to beginning
+TwoDArray.unshift()
+
+var myList = [['cereal', 3, ['milk', 5], ['juice', 25]]];
 
 
 
